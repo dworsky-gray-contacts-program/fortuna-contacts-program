659fe4d7c12c81c047a6239b64fc65fd5537b9ff
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-04-05 00:14:49 -0500

    refactored gotomainmenu method, started on addcontact method, refactored placement of multiple variables

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -8,40 +8,27 @@ import java.util.Scanner;
 
 public class MainMenu {
 
+    // Initialize Scanner
+    // private static Scanner scanner;
+    private static Scanner scanner = new Scanner(System.in);
+
+    // Set Scanner
+    // public static void SetScanner(Scanner input) {scanner = input;}
+
+    // Establish directory and file names
+    // Should never change, so have them as 'final'
+    final static String directory = "data";
+    final static String filename = "contacts.txt";
+
+    // Establish path to directory and file(s)
+    static Path dataDirectory = Paths.get(directory);
+    static Path contactsPath = Paths.get(directory, filename);
+
+    // Begin Main
     public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
+        int menuSelection;
 
-        // Establish directory and file names - should never change, so have them as final
-        final String directory = "data";
-        final String filename = "contacts.txt";
-
-        // Establish paths to directory and files within directory
-        Path dataDirectory = Paths.get(directory);
-        Path contactsPath = Paths.get(directory, filename);
-
-        // Check if pathway to directory exists - if it doesn't, create it
-        if (Files.notExists(dataDirectory)) {
-            Files.createDirectories(dataDirectory);
-        }
-        // Check if file in directory exists - if it doesn't, create it
-        if (!Files.exists(contactsPath)) {
-            Files.createFile(contactsPath);
-        }
-
-
-        // METHOD USING ARRAYLIST TO STORE INFORMATION
-        // Create new ArrayList
-        List<String> contacts = new ArrayList<>();
-        // Add to List
-        contacts.add("John Doe | 1234567881");
-        contacts.add("Jane Doe | 1569012833");
-
-        // Write to contacts.txt
-        Files.write(contactsPath, contacts);
-
-
-        int menuSelection = 0;
         do {
             try {
                 System.out.println("\n\t- - - - - Main Menu - - - - -");
@@ -60,7 +47,6 @@ public class MainMenu {
         }
         while (true);
 
-        // SWITCH FOR MENU OPTIONS
         switch (menuSelection) {
             case 1:
                 viewContacts();
@@ -69,7 +55,7 @@ public class MainMenu {
                 searchContacts();
                 break;
             case 3:
-                addContact();
+//                addContact();
                 break;
             case 4:
                 deleteContact();
@@ -79,7 +65,6 @@ public class MainMenu {
 
     // VIEW ALL CONTACTS IN LIST
     public static void viewContacts() throws IOException {
-        Path contactsPath = Paths.get("data", "contacts.txt");
         List<String> contacts = Files.readAllLines(contactsPath);
 
         System.out.println("\n\t- - - - - All Contacts - - - - -");
@@ -92,13 +77,13 @@ public class MainMenu {
 
     // SEARCH FOR CONTACT IN LIST
     public static void searchContacts() throws IOException {
-        Scanner scanner = new Scanner(System.in);
         String userSearch;
         String userContinue;
 
         do {
             System.out.println("\n\t- - - - - Search Contacts - - - - -");
             System.out.println("\tEnter Name");
+            scanner.nextLine();
             userSearch = scanner.nextLine().toLowerCase();
 
             Path contactsPath = Paths.get("data", "contacts.txt");
@@ -123,14 +108,52 @@ public class MainMenu {
             } else {
                 System.out.println("Invalid input. Please enter [Y]es or [N]o.");
             }
-            promptMainMenu();
         } while (true);
+        promptMainMenu();
     }
 
     // ADD CONTACT TO LIST
-    public static void addContact() {
-
-    }
+//    public static void addContact() throws IOException {
+//        String name;
+//        String lastName;
+//        String phoneNum;
+//
+//        System.out.println("Please enter a first name.");
+//        scanner.nextLine();
+//        name = scanner.nextLine().trim()+" ";
+//        System.out.println("Please enter a last name.");
+//        lastName = scanner.nextLine().trim();
+//
+//        name = name.concat(lastName+" | ");
+//        System.out.println(name);
+//
+//        do {
+//            System.out.println("Please enter a 10-digit phone number, starting with the area code.");
+//            phoneNum = scanner.nextLine();
+//
+//
+//        } while (! phoneNum.length() == 10);
+//
+//
+//        // Check if pathway to directory exists - if it doesn't, create it
+//        if (Files.notExists(dataDirectory)) {
+//            Files.createDirectories(dataDirectory);
+//        }
+//        // Check if file in directory exists - if it doesn't, create it
+//        if (!Files.exists(contactsPath)) {
+//            Files.createFile(contactsPath);
+//        }
+//
+//        // METHOD USING ARRAYLIST TO STORE INFORMATION
+//        // Create new ArrayList
+//        List<String> contacts = new ArrayList<>();
+//        // Add to List
+//        contacts.add("John Doe | 1234567881");
+//        contacts.add("Jane Doe | 1569012833");
+//
+//        // Write to contacts.txt
+//        Files.write(contactsPath, contacts);
+//    }
 
     // DELETE CONTACT FROM LIST
     public static void deleteContact() {
@@ -139,22 +162,29 @@ public class MainMenu {
 
     // PROMPT TO GO BACK TO MAIN MENU
     public static void promptMainMenu() throws IOException {
-        Scanner scanner = new Scanner(System.in);
         String goToMain;
+        String exit;
 
-        do {
-            System.out.println("\nReturn to Main Menu? [Y/N]");
-            goToMain = scanner.next();
+        System.out.println("\nReturn to Main Menu? [Y/N]");
+        goToMain = scanner.next();
 
-            if (goToMain.equalsIgnoreCase("y")) {
-                main(null);
-                break;
-            } else if (goToMain.equalsIgnoreCase("n")) {
+        if (goToMain.equalsIgnoreCase("y")) {
+            main(null);
+        } else if (goToMain.equalsIgnoreCase("n")) {
+            System.out.println("Exit program? [Y/N]");
+            exit = scanner.next();
+
+            if (exit.equalsIgnoreCase("y")) {
                 System.out.println("Exiting program. Goodbye.");
-                break;
+            } else if (exit.equalsIgnoreCase("n")) {
+                promptMainMenu();
             } else {
-                System.out.println("Invalid entry.");
+                System.out.println("Invalid input. Please try again.");
+                promptMainMenu();
             }
-        } while (true);
+        } else {
+            System.out.println("Invalid entry.");
+            promptMainMenu();
+        }
     }
 }
